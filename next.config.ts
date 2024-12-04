8a24c1222a4bd9bd121ca4eaecdc9bf75175eb8a
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-04 16:01:38 +0700

    update load image

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
-const isDev = process.env.NODE_ENV === 'development';
-const repoName = 'nextjs-page-router';
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
-  basePath: isDev ? '' : `/${repoName}`,
-  assetPrefix: isDev ? '' : `/${repoName}`,
 };
 
 export default nextConfig;
