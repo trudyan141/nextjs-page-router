9c153bd507d4f6b303d9d8b987d72fc027ed503c
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-04 15:40:43 +0700

    update source load with env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,32 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Script from "next/script";
+import { useState , useEffect} from "react";
 declare const TE;
+
 export default function App({ Component, pageProps }: AppProps) {
+   const [env, setEnv] = useState<string | null>(null);
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const envParam = urlParams.get("env") || "prod";
+    setEnv(envParam);
+  }, []);
+  const scriptSrc =
+    env === "dev"
+      ? "https://tma-demo.dmtp.tech/sdk/0.0.8/bec.js?walletAddress=QnLOYksIDhA3MfBLoRL%2ByIa8jRggeovB3NtN3d7LD7g%3D"
+      : "https://bec.dmtp.tech/0.0.8/bec.js?walletAddress=QnLOYksIDhA3MfBLoRL%2ByIa8jRggeovB3NtN3d7LD7g%3D";
   return <>
-       <Script
-        src="https://tma-demo.dmtp.tech/sdk/0.0.8/bec.js?walletAddress=39oXQfuzAVeV2QaqYzmAWC8w9sgl7Bvvhu%2FnrBf2bYw%3D"
-        strategy="afterInteractive" // Loads the script after the page is interactive
-        onLoad={() => {
-          if (typeof TE.onLoad === "function") {
-              TE.onLoad();
-          } else {
-            console.error('TE.onLoad is not a function');
-          }
-        }}
-      />
+    {env && <Script
+      src={scriptSrc}
+      strategy="afterInteractive" // Loads the script after the page is interactive
+      onLoad={() => {
+        if (typeof TE.onLoad === "function") {
+          TE.onLoad();
+        } else {
+          console.error('TE.onLoad is not a function');
+        }
+      }}
+    />}
      <Component {...pageProps} />;
   </>
  
