7f701d0fb3dd3343baab78519a4128f00cb2eac2
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-04 15:49:50 +0700

    chore fix wrong repo name

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,6 @@
 import type { NextConfig } from "next";
 const isDev = process.env.NODE_ENV === 'development';
-const repoName = 'nextjs-app-router';
+const repoName = 'nextjs-page-router';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
