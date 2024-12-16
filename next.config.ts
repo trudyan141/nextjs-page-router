117788049d372122b4a79458b79d15ab3f795fa0
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-16 16:58:10 +0700

    update image again

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig : NextConfig = {
-  basePath: isProd ? '/nextjs-page-router' : '',
-  trailingSlash: true, 
-  assetPrefix: isProd ? '/nextjs-page-router/' : '',
-  images: {
-    unoptimized: true, 
-  }
+  reactStrictMode: true,
 };
 
 module.exports = nextConfig;
