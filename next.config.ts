f25d24eb972001ef594adaf31d5d467e47076b67
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-16 16:41:30 +0700

    test path again

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 const nextConfig : NextConfig = {
   basePath: isProd ? '/nextjs-page-router' : '',
   trailingSlash: isProd, 
-  assetPrefix: isProd ? '/nextjs-page-router' : ''
+  assetPrefix: isProd ? '/nextjs-page-router/' : ''
 };
 
 module.exports = nextConfig;
