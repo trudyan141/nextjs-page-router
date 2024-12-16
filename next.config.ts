f52847c7cf05c26f357aaafeec950b3b8027ab6e
trunglk-ibl <trunglk@blockchainlabs.asia>
2024-12-16 16:45:40 +0700

    test path config again

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,11 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 const nextConfig : NextConfig = {
   basePath: isProd ? '/nextjs-page-router' : '',
-  trailingSlash: isProd, 
-  assetPrefix: isProd ? '/nextjs-page-router/' : ''
+  trailingSlash: true, 
+  assetPrefix: isProd ? '/nextjs-page-router/' : '',
+  images: {
+    unoptimized: true, 
+  }
 };
 
 module.exports = nextConfig;
